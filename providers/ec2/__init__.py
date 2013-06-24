4d10f949265a0ec1eae3b29fe81ab23fb409272f
Anders Ingemann <anders@ingemann.de>
2013-06-24 23:03:43 +0200

    Check for EBS backing the proper way

--- providers/ec2/__init__.py
+++ providers/ec2/__init__.py
@@ -8,5 +8,5 @@ def tasks(tasklist, manifest):
 	from tasks import ebs
 	tasklist.add(packages.HostPackages(), packages.ImagePackages(), host.CheckPackages(),
 	             connection.GetCredentials(), host.GetInfo(), connection.Connect())
-	if manifest.volume['backing'] is 'ebs':
-		tasklist.add(ebs.CreateVolume())
+	if manifest.volume['backing'].lower() == 'ebs':
+		tasklist.add(ebs.CreateVolume(), ebs.AttachVolume())
