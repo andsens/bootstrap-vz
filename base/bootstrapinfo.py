59bae6e497e45ff6f7d8ba02b8dc1855b4ca73f0
Anders Ingemann <anders@ingemann.de>
2013-07-13 15:01:56 +0200

    Create unique identifier in BootstrapInformation Can be used to uniquely name temporary resources

--- base/bootstrapinfo.py
+++ base/bootstrapinfo.py
@@ -3,4 +3,6 @@
 class BootstrapInformation(object):
 	def __init__(self, manifest=None, debug=False):
 		self.manifest = manifest
-		self.debug    = debug
+		self.debug = debug
+		import random
+		self.run_id = random.randrange(16**8)
