264f33d0e548dd1497085183cccc2814fdb678ab
Anders Ingemann <anders@ingemann.de>
2013-08-11 18:11:46 +0200

    We do not need the credentials stanza for virtualbox

--- providers/virtualbox/manifest.py
+++ providers/virtualbox/manifest.py
@@ -10,7 +10,6 @@ class Manifest(base.Manifest):
 
 	def parse(self, data):
 		super(Manifest, self).parse(data)
-		self.credentials    = data['credentials']
 		self.virtualization = data['virtualization']
 		self.image          = data['image']
 		if 'loopback_dir' not in self.volume and self.volume['backing'].lower() == 's3':
