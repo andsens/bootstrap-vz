3dd3e00e5c946d839d6903fc2cc4eeadb552be68
Olivier Sallou <olivier.sallou@debian.org>
2013-08-13 10:36:32 +0200

    add plugin to convert raw images to vdi,qcow etc...

--- common/phases.py
+++ common/phases.py
@@ -9,6 +9,7 @@ system_modification = Phase('System modification', 'Installing software, modifyi
 system_cleaning = Phase('System cleaning', 'Removing sensitive data, temporary files and other leftovers')
 volume_unmounting = Phase('Volume unmounting', 'Unmounting the bootstrap volume')
 image_registration = Phase('Image registration', 'Uploading/Registering with the provider')
+image_conversion = Phase('Image conversion', 'Conversion/Compression of the image result file')
 cleaning = Phase('Cleaning', 'Removing temporary files')
 
 order = [preparation,
@@ -20,5 +21,6 @@ order = [preparation,
          system_cleaning,
          volume_unmounting,
          image_registration,
+	 image_conversion,
          cleaning,
          ]
---
+++ plugins/convert_image/__init__.py
@@ -0,0 +1,11 @@
+
+
+def tasks(tasklist, manifest):
+	from tasks import ConvertImage
+	tasklist.add(ConvertImage())
+
+
+def validate_manifest(data, schema_validate):
+	from os import path
+	schema_path = path.normpath(path.join(path.dirname(__file__), 'manifest-schema.json'))
+	schema_validate(data, schema_path)
