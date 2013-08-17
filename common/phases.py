e6ec040b276823e22df7060a3a4c06e576d467e3
Anders Ingemann <anders@ingemann.de>
2013-08-17 22:54:24 +0200

    Remove image conversion phase. Maybe image_registration should be renamed?

--- common/phases.py
+++ common/phases.py
@@ -9,7 +9,6 @@ system_modification = Phase('System modification', 'Installing software, modifyi
 system_cleaning = Phase('System cleaning', 'Removing sensitive data, temporary files and other leftovers')
 volume_unmounting = Phase('Volume unmounting', 'Unmounting the bootstrap volume')
 image_registration = Phase('Image registration', 'Uploading/Registering with the provider')
-image_conversion = Phase('Image conversion', 'Conversion/Compression of the image result file')
 cleaning = Phase('Cleaning', 'Removing temporary files')
 
 order = [preparation,
@@ -21,6 +20,5 @@ order = [preparation,
          system_cleaning,
          volume_unmounting,
          image_registration,
-         image_conversion,
          cleaning,
          ]
