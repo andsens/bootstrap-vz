c924d500fe36ef71a1104868c17df81091e4557f
Olivier Sallou <olivier.sallou@irisa.fr>
2013-09-04 14:46:54 +0200

    add new plugin image_commands to execute cmd or scripts in image

---
+++ plugins/image_commands/__init__.py
@@ -0,0 +1,11 @@
+
+
+def tasks(tasklist, manifest):
+        from image_commands import ImageExecuteCommand
+        tasklist.add(ImageExecuteCommand())
+
+def validate_manifest(data, schema_validate):
+        from os import path
+        schema_path = path.normpath(path.join(path.dirname(__file__), 'manifest-schema.json'))
+        schema_validate(data, schema_path)
+
