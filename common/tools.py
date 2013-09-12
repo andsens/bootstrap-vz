c1e931df5e802041518c3abce82401b922c0a2a9
Anders Ingemann <anders@ingemann.de>
2013-09-13 01:32:08 +0200

    Fix bug where log_call would not read all lines

--- common/tools.py
+++ common/tools.py
@@ -31,13 +31,11 @@ def log_call(command, stdin=None):
 		ret = select.select(reads, [], [])
 		for fd in ret[0]:
 			if fd == process.stdout.fileno():
-				line = process.stdout.readline()
-				if line != '':
+				for line in iter(process.stdout.readline, ''):
 					log.debug(line.strip())
 					stdout.append(line.strip())
 			if fd == process.stderr.fileno():
-				line = process.stderr.readline()
-				if line != '':
+				for line in iter(process.stderr.readline, ''):
 					log.error(line.strip())
 					stderr.append(line.strip())
 		if process.poll() is not None:
