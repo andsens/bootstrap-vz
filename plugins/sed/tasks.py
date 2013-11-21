d05c97afef09bc06efe843799f0cf35219389f41
Anders Ingemann <aim@secoya.dk>
2013-11-21 15:52:26 +0100

    Fix indentation for sed plugin

--- plugins/sed/tasks.py
+++ plugins/sed/tasks.py
@@ -6,10 +6,10 @@ import os
 
 
 class DoSeds(Task):
-        description = 'Sedding files'
-        phase = phases.system_modification
+	description = 'Sedding files'
+	phase = phases.system_modification
 	after = [AptUpgrade]
-        def run(self, info):
 
-			chroot_path = os.path.join(info.root, info.manifest.plugins['sed']['file'])
-			sed_i(chroot_path, info.manifest.plugins['sed']['find'], info.manifest.plugins['sed']['replace'])
+	def run(self, info):
+		chroot_path = os.path.join(info.root, info.manifest.plugins['sed']['file'])
+		sed_i(chroot_path, info.manifest.plugins['sed']['find'], info.manifest.plugins['sed']['replace'])
