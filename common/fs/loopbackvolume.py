8165297a0285fa440db37d6fd85fb80f4546fe12
Anders Ingemann <anders@ingemann.de>
2014-01-18 16:33:41 +0100

    Generalize link_dm_node() it works for all devices Also accept some new parameters to adjust offsets and startsecotrs when creating the disk mapping

--- common/fs/loopbackvolume.py
+++ common/fs/loopbackvolume.py
@@ -1,17 +1,8 @@
 from base.fs.volume import Volume
 from common.tools import log_check_call
-from base.fs.exceptions import VolumeError
 
 
 class LoopbackVolume(Volume):
-
-	events = [{'name': 'create', 'src': 'nonexistent', 'dst': 'detached'},
-	          {'name': 'attach', 'src': 'detached', 'dst': 'attached'},
-	          {'name': 'link_dm_node', 'src': 'attached', 'dst': 'linked'},
-            {'name': 'unlink_dm_node', 'src': 'linked', 'dst': 'attached'},
-	          {'name': 'detach', 'src': 'attached', 'dst': 'detached'},
-	          {'name': 'delete', 'src': 'detached', 'dst': 'deleted'},
-	          ]
 
 	extension = 'raw'
 
@@ -24,42 +15,6 @@ class LoopbackVolume(Volume):
 
 	def _before_attach(self, e):
 		[self.loop_device_path] = log_check_call(['/sbin/losetup', '--show', '--find', self.image_path])
-		self.device_path = self.loop_device_path
-
-	def _before_link_dm_node(self, e):
-		import os.path
-		from . import get_partitions
-		proc_partitions = get_partitions()
-		loop_device_name = os.path.basename(self.loop_device_path)
-		loop_device_partition = proc_partitions[loop_device_name]
-
-		sectors = self.size * 1024 * 1024 / 512
-		table = ('{log_start_sec} {sectors} linear {major}:{minor} {start_sec}'
-		         .format(log_start_sec=0,
-		                 sectors=sectors,
-		                 major=loop_device_partition['major'],
-		                 minor=loop_device_partition['minor'],
-		                 start_sec=0))
-		import string
-		import os.path
-		for letter in string.ascii_lowercase:
-			dev_name = 'vd' + letter
-			dev_path = os.path.join('/dev/mapper', dev_name)
-			if not os.path.exists(dev_path):
-				self.dm_node_name = dev_name
-				self.dm_node_path = dev_path
-				break
-
-		if not hasattr(self, 'dm_node_name'):
-			raise VolumeError('Unable to find a free block device path for mounting the bootstrap volume')
-
-		log_check_call(['/sbin/dmsetup', 'create', self.dm_node_name], table)
-		self.device_path = self.dm_node_path
-
-	def _before_unlink_dm_node(self, e):
-		log_check_call(['/sbin/dmsetup', 'remove', self.dm_node_name])
-		del self.dm_node_name
-		del self.dm_node_path
 		self.device_path = self.loop_device_path
 
 	def _before_detach(self, e):
