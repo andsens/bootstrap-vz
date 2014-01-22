d85ad7f5980d2af34ea366b6299514b848205a30
Anders Ingemann <anders@ingemann.de>
2014-01-22 01:03:30 +0100

    Minimize size plugin can now shrink vmdk images It requires vmware workstation to be installed (and zerofree optionally, but highly recommended)

--- plugins/minimize_size/tasks.py
+++ plugins/minimize_size/tasks.py
@@ -1,7 +1,10 @@
 from base import Task
 from common import phases
-from common.tasks import bootstrap
 from common.tasks import apt
+from common.tasks import bootstrap
+from common.tasks import filesystem
+from common.tasks import partitioning
+from common.tasks import volume
 import os
 
 folders = ['tmp', 'var/lib/apt/lists']
@@ -41,3 +44,58 @@ class RemoveFolderMounts(Task):
 
 		os.rmdir(info.minimize_size_folder)
 		del info.minimize_size_folder
+
+
+class CheckZerofreePath(Task):
+	description = 'Checking path to zerofree tool'
+	phase = phases.preparation
+
+	@classmethod
+	def run(cls, info):
+		from common.exceptions import TaskError
+		import os
+		zerofree = info.manifest.plugins['minimize_size']['zerofree']
+		if not os.path.isfile(zerofree):
+			raise TaskError('The path `{path}\' does not exist or is not a file'.format(path=zerofree))
+		if not os.access(zerofree, os.X_OK):
+			raise TaskError('The path `{path}\' is not executable'.format(path=zerofree))
+
+
+# Get zerofree here: http://intgat.tigress.co.uk/rmy/uml/index.html
+class Zerofree(Task):
+	description = 'Zeroing unused blocks on the volume'
+	phase = phases.volume_unmounting
+	predecessors = [filesystem.UnmountRoot, partitioning.UnmapPartitions]
+	successors = [volume.Detach]
+
+	@classmethod
+	def run(cls, info):
+		from common.tools import log_check_call
+		zerofree = info.manifest.plugins['minimize_size']['zerofree']
+		log_check_call([zerofree, info.volume.device_path])
+
+
+class CheckVMWareDMCommand(Task):
+	description = 'Checking path to vmware-vdiskmanager tool'
+	phase = phases.preparation
+
+	@classmethod
+	def run(cls, info):
+		from common.exceptions import TaskError
+		import os
+		vdiskmngr = '/usr/bin/vmware-vdiskmanager'
+		if not os.path.isfile(vdiskmngr):
+			raise TaskError('Unable to find vmware-vdiskmanager at `{path}\''.format(path=vdiskmngr))
+		if not os.access(vdiskmngr, os.X_OK):
+			raise TaskError('vmware-vdiskmanager at `{path}\' is not executable'.format(path=vdiskmngr))
+
+
+class ShrinkVolume(Task):
+	description = 'Shrinking the volume'
+	phase = phases.volume_unmounting
+	predecessors = [volume.Detach]
+
+	@classmethod
+	def run(cls, info):
+		from common.tools import log_check_call
+		log_check_call(['/usr/bin/vmware-vdiskmanager', '-k', info.volume.image_path])
