29d4bdf70d062a221a9a344b4ac2e27cc928a41e
Anders Ingemann <anders@ingemann.de>
2014-02-18 00:07:26 +0100

    minor cleanup in kvm provider

--- providers/kvm/tasks/virtio.py
+++ providers/kvm/tasks/virtio.py
@@ -1,7 +1,5 @@
 from base import Task
 from common import phases
-from common.tasks.packages import InstallPackages
-from common.exceptions import TaskError
 import os
 
 
@@ -11,9 +9,8 @@ class VirtIO(Task):
 
 	@classmethod
 	def run(cls, info):
-		from common.tools import log_call
-		modules = os.path.join(info.root,'/etc/initramfs-tools/modules')
+		modules = os.path.join(info.root, '/etc/initramfs-tools/modules')
 		with open(modules, "a") as modules_file:
-			modules_file.write("\n");
+			modules_file.write("\n")
 			for module in info.manifest.bootstrapper.get('virtio', []):
 				modules_file.write(module+"\n")
