afffdf680d57eb73f6281c6b015ab55b39f09e6d
Tiago Ilieve <tiago.myhro@gmail.com>
2014-03-28 17:58:45 -0300

    New plugin: ntp

---
+++ plugins/ntp/__init__.py
@@ -0,0 +1,11 @@
+def validate_manifest(data, validator, error):
+	import os.path
+	schema_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'manifest-schema.json'))
+	validator(data, schema_path)
+
+
+def resolve_tasks(taskset, manifest):
+	import tasks
+	taskset.add(tasks.AddNtpPackage)
+	if manifest.plugins['ntp'].get('servers', False):
+		taskset.add(tasks.SetNtpServers)
