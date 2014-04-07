fb5d630c9b6ea422fbf9d40c72a9c92f8b6c2ca4
Olivier Sallou <olivier.sallou@gmail.com>
2014-04-07 14:23:24 +0200

    add azure provider

---
+++ bootstrapvz/providers/azure/tasks/boot.py
@@ -0,0 +1,17 @@
+from bootstrapvz.base import Task
+from bootstrapvz.common import phases
+from bootstrapvz.common.tasks import boot
+
+class ConfigureGrub(Task):
+	description = 'Change grub configuration to allow for ttyS0 output'
+	phase = phases.system_modification
+	successors = [boot.InstallGrub]
+
+	@classmethod
+	def run(cls, info):
+		import os
+		from bootstrapvz.common.tools import sed_i
+		grub_config = os.path.join(info.root, 'etc/default/grub')
+		sed_i(grub_config, r'^(GRUB_CMDLINE_LINUX*=".*)"\s*$', r'\1console=ttyS0 earlyprintk=ttyS0 rootdelay=300"')
+		sed_i(grub_config, r'^.*(GRUB_TIMEOUT=).*$', r'GRUB_TIMEOUT=0')
+
---
+++ bootstrapvz/providers/azure/tasks/image.py
@@ -0,0 +1,26 @@
+from bootstrapvz.base import Task
+from bootstrapvz.common import phases
+from bootstrapvz.common.tasks import loopback
+
+
+class ConvertToVhd(Task):
+	description = 'Convert raw image to vhd disk'
+	phase = phases.image_registration
+
+	@classmethod
+	def run(cls, info):
+		image_name = info.manifest.image['name'].format(**info.manifest_vars)
+		filename = '{image_name}.{ext}'.format(image_name=image_name, ext='vhd')
+		import os.path
+		destination = os.path.join(info.manifest.bootstrapper['workspace'], filename)
+
+		file_size = os.path.getsize(info.volume.image_path)
+		rounded_vol_size = str(((file_size/(1024*1024)+1)*(1024*1024)))
+
+		from bootstrapvz.common.tools import log_check_call
+		log_check_call(['qemu-img', 'resize', info.volume.image_path, rounded_vol_size])
+		log_check_call(['qemu-img', 'convert', '-o', 'subformat=fixed', '-O', 'vpc', info.volume.image_path, destination])
+		os.remove(info.volume.image_path)
+		import logging
+		log = logging.getLogger(__name__)
+		log.info('The volume image has been moved to {image_path}'.format(image_path=destination))
