b327823261d7ed5e782dc396143c9430c138d9b6
Tomasz Rybak <tomasz.rybak@post.pl>
2014-05-02 17:36:08 +0200

    Add Google Cloud Engine (GCE) provider:
     * force kpartx to use synchronous mode, removing sleep(10)
     * get image configuration, use it during tarball creation and registration
     * add (non-working, path problems) image registration
     * add cleaning of image from Google keys and repositories
     * add NTP server address in manifest
     * add preference for backport kernels in manifest
     * disable IPv6
     * correctly set host name

---
+++ bootstrapvz/providers/gce/tasks/apt.py
@@ -0,0 +1,56 @@
+from bootstrapvz.base import Task
+from bootstrapvz.common import phases
+from bootstrapvz.common.tasks import apt
+from bootstrapvz.common.tools import log_check_call
+import os
+
+
+class SetPackageRepositories(Task):
+	description = 'Adding apt sources'
+	phase = phases.preparation
+	successors = [apt.AddManifestSources]
+
+	@classmethod
+	def run(cls, info):
+		sections = 'main'
+		if 'sections' in info.manifest.system:
+			sections = ' '.join(info.manifest.system['sections'])
+		info.source_lists.add('main', 'deb     http://http.debian.net/debian {system.release} ' + sections)
+		info.source_lists.add('main', 'deb-src http://http.debian.net/debian {system.release} ' + sections)
+		info.source_lists.add('backports', 'deb     http://http.debian.net/debian {system.release}-backports ' + sections)
+		info.source_lists.add('backports', 'deb-src http://http.debian.net/debian {system.release}-backports ' + sections)
+		info.source_lists.add('goog', 'deb http://goog-repo.appspot.com/debian pigeon main')
+
+
+class ImportGoogleKey(Task):
+	description = 'Adding Google key'
+	phase = phases.package_installation
+	predecessors = [apt.InstallTrustedKeys]
+	successors = [apt.WriteSources]
+
+	@classmethod
+	def run(cls, info):
+		key_file = os.path.join(info.root, 'google.gpg.key')
+		log_check_call(['wget', 'https://goog-repo.appspot.com/debian/key/public.gpg.key', '-O', key_file])
+		log_check_call(['chroot', info.root, 'apt-key', 'add', 'google.gpg.key'])
+		os.remove(key_file)
+
+
+class CleanGoogleRepositoriesAndKeys(Task):
+	description = 'Removing Google key and apt source files'
+	phase = phases.system_cleaning
+	successors = [apt.AptClean]
+
+	@classmethod
+	def run(cls, info):
+		keys = log_check_call(['chroot', info.root, 'apt-key',
+		                       'adv', '--with-colons', '--list-keys'])
+		# protect against first lines with debug information,
+		# not apt-key output
+		key_id = [key.split(':')[4] for key in keys
+		          if len(key.split(':')) == 13 and
+		          key.split(':')[9].find('@google.com') > 0]
+		log_check_call(['chroot', info.root, 'apt-key', 'del', key_id[0]])
+		apt_file = os.path.join(info.root, 'etc/apt/sources.list.d/goog.list')
+		os.remove(apt_file)
+		log_check_call(['chroot', info.root, 'apt-get', 'update'])
---
+++ bootstrapvz/providers/gce/tasks/image.py
@@ -0,0 +1,61 @@
+from bootstrapvz.base import Task
+from bootstrapvz.common import phases
+from bootstrapvz.common.tasks import loopback
+from bootstrapvz.common.tools import log_check_call
+import os.path
+
+
+class CreateTarball(Task):
+	description = 'Creating tarball with image'
+	phase = phases.image_registration
+	predecessors = [loopback.MoveImage]
+
+	@classmethod
+	def run(cls, info):
+		import datetime
+		image_name = info.manifest.image['name'].format(**info.manifest_vars)
+		filename = '{image_name}.{ext}'.format(image_name=image_name, ext=info.volume.extension)
+		today = datetime.datetime.today()
+		name_suffix = today.strftime('%Y%m%d')
+		image_name_format = '{lsb_distribution}-{lsb_release}-{release}-v{name_suffix}'
+		image_name = image_name_format.format(lsb_distribution=info._gce['lsb_distribution'],
+		                                      lsb_release=info._gce['lsb_release'],
+		                                      release=info.manifest.system['release'],
+		                                      name_suffix=name_suffix)
+		# ensure that we do not use disallowed characters in image name
+		image_name = image_name.lower()
+		image_name = image_name.replace(".", "-")
+		info._gce['image_name'] = image_name
+		tarball_name = '{image_name}.tar.gz'.format(image_name=image_name)
+		tarball_path = os.path.join(info.manifest.bootstrapper['workspace'], tarball_name)
+		info._gce['tarball_name'] = tarball_name
+		info._gce['tarball_path'] = tarball_path
+		log_check_call(['tar', '--sparse', '-C', info.manifest.bootstrapper['workspace'],
+		                '-caf', tarball_path, filename])
+
+
+class UploadImage(Task):
+	description = 'Uploading image to GSE'
+	phase = phases.image_registration
+	predecessors = [CreateTarball]
+
+	@classmethod
+	def run(cls, info):
+		log_check_call(['gsutil', 'cp', info._gce['tarball_path'],
+		                info.manifest.image['gcs_destination'] + info._gce['tarball_name']])
+
+
+class RegisterImage(Task):
+	description = 'Registering image with GCE'
+	phase = phases.image_registration
+	predecessors = [UploadImage]
+
+	@classmethod
+	def run(cls, info):
+		image_description = info._gce['lsb_description']
+		if 'description' in info.manifest.image:
+			image_description = info.manifest.image['description']
+		log_check_call(['gcutil', '--project={}'.format(info.manifest.image['gce_project']),
+		                'addimage', info._gce['image_name'],
+		                info.manifest.image['gcs_destination'] + info._gce['tarball_name'],
+		                '--description={}'.format(image_description)])
