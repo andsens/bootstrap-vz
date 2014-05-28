669ccc3a8ba4b6c0a3fabfc1e29c2edd3e1d2a88
Ilya Margolin <ilya@jimdo.com>
2014-05-28 19:00:35 +0200

    Added pip_install plugin
    configuration is like
    
    "pip_install": {"packages": ["a_package", "another_package==1.0.1"]}
    
    Installs build-essential and python-dev

---
+++ bootstrapvz/plugins/pip_install/__init__.py
@@ -0,0 +1,12 @@
+import tasks
+
+
+def validate_manifest(data, validator, error):
+	import os.path
+	schema_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'manifest-schema.json'))
+	validator(data, schema_path)
+
+
+def resolve_tasks(taskset, manifest):
+	taskset.add(tasks.AddPipPackage)
+	taskset.add(tasks.PipInstallCommand)
