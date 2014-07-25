719a6c31b00778343f61800064c52122be592a96
Tiago Ilieve <tiago.myhro@gmail.com>
2014-07-25 00:08:07 -0300

    Reverting 210999f, as asked by @jkaplowitz

--- bootstrapvz/providers/gce/tasks/apt.py
+++ bootstrapvz/providers/gce/tasks/apt.py
@@ -6,12 +6,19 @@ import os
 
 
 class SetPackageRepositories(Task):
-	description = 'Adding goog apt source'
+	description = 'Adding apt sources'
 	phase = phases.preparation
 	successors = [apt.AddManifestSources]
 
 	@classmethod
 	def run(cls, info):
+		components = 'main'
+		if 'components' in info.manifest.system:
+			components = ' '.join(info.manifest.system['components'])
+		info.source_lists.add('main', 'deb     http://http.debian.net/debian {system.release} ' + components)
+		info.source_lists.add('main', 'deb-src http://http.debian.net/debian {system.release} ' + components)
+		info.source_lists.add('backports', 'deb     http://http.debian.net/debian {system.release}-backports ' + components)
+		info.source_lists.add('backports', 'deb-src http://http.debian.net/debian {system.release}-backports ' + components)
 		info.source_lists.add('goog', 'deb http://goog-repo.appspot.com/debian pigeon main')
 
 
