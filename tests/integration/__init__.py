03a48b940772706595cba979e10844625e035b73
Anders Ingemann <anders@ingemann.de>
2014-11-25 21:59:12 +0100

    Simplify loading of partial manifests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,9 +1,11 @@
 import os.path
 from bootstrapvz.common.tools import load_data
 
-combine_manifests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'manifests')
-manifests = {'base': load_data(os.path.join(combine_manifests_path, 'base.yml')),
-             'partitioned': load_data(os.path.join(combine_manifests_path, 'partitioned.yml')),
-             'unpartitioned': load_data(os.path.join(combine_manifests_path, 'unpartitioned.yml')),
-             }
+partial_manifests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'manifests')
+import glob
+partial_yaml = glob.glob(os.path.join(partial_manifests_path, '*.yml'))
+partial_json = glob.glob(os.path.join(partial_manifests_path, '*.json'))
+manifests = {}
+for path in partial_yaml + partial_json:
+	manifests[os.path.basename(path)] = load_data(path)
 build_settings = load_data(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'build_settings.yml'))
