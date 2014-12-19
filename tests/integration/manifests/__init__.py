71e0d943fcb08c2671ca51f53db501a7f72fe947
Anders Ingemann <anders@ingemann.de>
2014-12-19 01:26:33 +0100

    Make json and yaml files conflict instead of override

--- tests/integration/manifests/__init__.py
+++ tests/integration/manifests/__init__.py
@@ -5,11 +5,11 @@ from bootstrapvz.common.tools import load_data
 partial_json = glob.glob(os.path.join(os.path.dirname(__file__), '*.yml'))
 partial_yaml = glob.glob(os.path.join(os.path.dirname(__file__), '*.json'))
 
-def dictkey(path):
-	return 
-# yaml overrides json
 partials = {}
 for path in partial_json + partial_yaml:
 	key = os.path.splitext(os.path.basename(path))[0]
+	if key in partials:
+		msg = 'Error when loading partial manifests: The partial {key} exists twice'.format(key=key)
+		raise Exception(msg)
 	partials[key] = load_data(path)
 	
