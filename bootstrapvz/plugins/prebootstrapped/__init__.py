e1946fcb685986c9482066cfea321740e72ff023
Anders Ingemann <anders@ingemann.de>
2015-01-15 22:44:55 +0100

    Simpler check for whether to create image or use image in prebootstrapped plugin

--- bootstrapvz/plugins/prebootstrapped/__init__.py
+++ bootstrapvz/plugins/prebootstrapped/__init__.py
@@ -37,13 +37,13 @@ def resolve_tasks(taskset, manifest):
 	              guest_additions.InstallGuestAdditions,
 	              ]
 	if manifest.volume['backing'] == 'ebs':
-		if 'snapshot' in settings and settings['snapshot'] is not None:
+		if settings.get('snapshot', None) is not None:
 			taskset.add(CreateFromSnapshot)
 			[taskset.discard(task) for task in skip_tasks]
 		else:
 			taskset.add(Snapshot)
 	else:
-		if 'image' in settings and settings['image'] is not None:
+		if settings.get('image', None) is not None:
 			taskset.add(CreateFromImage)
 			[taskset.discard(task) for task in skip_tasks]
 		else:
