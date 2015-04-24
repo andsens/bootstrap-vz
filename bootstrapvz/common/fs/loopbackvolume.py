cba0a99d0a5ab0c73de18ced58c4aca31fcadfea
Tiago Ilieve <tiago.myhro@gmail.com>
2015-04-23 23:19:16 -0300

    Fix `truncate` arguments
    `truncate` doesn't expect the parameter `create` that was used
    previously by `qemu-img`. This made a sparse file named "create" to
    appear at current working directory every time the command as executed.
    
    This closes #212.

--- bootstrapvz/common/fs/loopbackvolume.py
+++ bootstrapvz/common/fs/loopbackvolume.py
@@ -12,7 +12,7 @@ class LoopbackVolume(Volume):
 	def _before_create(self, e):
 		self.image_path = e.image_path
 		size_opt = '--size={mib}M'.format(mib=self.size.bytes.get_qty_in('MiB'))
-		log_check_call(['truncate', 'create', size_opt, self.image_path])
+		log_check_call(['truncate', size_opt, self.image_path])
 
 	def _before_attach(self, e):
 		[self.loop_device_path] = log_check_call(['losetup', '--show', '--find', self.image_path])
