774e78a9f25da147e49e48decb209b8d666513d7
Thomas Conte <thomas@conte.com>
2015-06-29 02:58:32 -0700

    Patch udev init script to fix ROOTDELAY boot issue

---
+++ bootstrapvz/providers/azure/tasks/udev.py
@@ -0,0 +1,19 @@
+from bootstrapvz.base import Task
+from bootstrapvz.common import phases
+from bootstrapvz.common.tasks import kernel
+import subprocess
+import os.path
+from . import assets
+
+
+class PatchUdev(Task):
+	description = 'Patch udev configuration to remove ROOTDELAY sleep'
+	phase = phases.system_modification
+	successors = [kernel.UpdateInitramfs]
+
+	@classmethod
+	def run(cls, info):
+		# c.f. http://anonscm.debian.org/cgit/pkg-systemd/systemd.git/commit/?id=61e055638cea
+		with open(os.path.join(assets, 'udev.diff')) as diff_file:
+			udev_dir = os.path.join(info.root, 'usr/share/initramfs-tools/scripts/init-top')
+			subprocess.call(['patch', '--no-backup-if-mismatch', '-p6', '-d' + udev_dir], stdin=diff_file)
