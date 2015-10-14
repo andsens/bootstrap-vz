d5ef584b4230d937f50566258f20208028317b70
Anders Ingemann <anders@ingemann.de>
2015-10-14 22:05:00 +0200

    Add note about why the VDI format does not have an OVF URI

--- bootstrapvz/common/fs/virtualdiskimage.py
+++ bootstrapvz/common/fs/virtualdiskimage.py
@@ -5,6 +5,7 @@ class VirtualDiskImage(QEMUVolume):
 
 	extension = 'vdi'
 	qemu_format = 'vdi'
+	# VDI format does not have an URI (check here: https://forums.virtualbox.org/viewtopic.php?p=275185#p275185)
 	ovf_uri = None
 
 	def get_uuid(self):
