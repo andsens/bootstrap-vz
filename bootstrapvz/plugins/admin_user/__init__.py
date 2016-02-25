72a9975ed9543ac70fadd31b2f863c5025b65a10
Nicolas Braud-Santoni <nicolas.braud-santoni@iaik.tugraz.at>
2016-02-25 20:38:42 +0100

    admin_user: Add warnings

--- bootstrapvz/plugins/admin_user/__init__.py
+++ bootstrapvz/plugins/admin_user/__init__.py
@@ -11,6 +11,7 @@ def validate_manifest(data, validator, error):
 
 
 def resolve_tasks(taskset, manifest):
+        import logging
 	import tasks
 	from bootstrapvz.common.tasks import ssh
 
@@ -21,10 +22,14 @@ def resolve_tasks(taskset, manifest):
 	if 'password' in manifest.plugins['admin_user']:
 		taskset.discard(ssh.DisableSSHPasswordAuthentication)
 		taskset.add(tasks.AdminUserPassword)
+
 	if 'pubkey' in manifest.plugins['admin_user']:
 		taskset.add(tasks.AdminUserPublicKey)
 	elif manifest.provider['name'] == 'ec2':
+		logging.getLogger(__name__).info("The SSH key will be obtained from EC2")
 		taskset.add(tasks.AdminUserPublicKeyEC2)
+	elif 'password' not in manifest.plugins['admin_user']:
+		logging.getLogger(__name__).warn("No SSH key and no password set")
 
 	taskset.update([tasks.AddSudoPackage,
 	                tasks.CreateAdminUser,
