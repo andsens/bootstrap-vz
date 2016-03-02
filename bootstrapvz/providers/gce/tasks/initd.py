6c03860eacfad9429e7d8ae435f39d3b01324295
Zach Marano <zmarano@google.com>
2016-03-02 12:46:23 -0800

    Fix GCE expand-root since commit/2ece5a66e1e36f30bef3a675e8287ab8c1bcc04f

--- bootstrapvz/providers/gce/tasks/initd.py
+++ bootstrapvz/providers/gce/tasks/initd.py
@@ -6,18 +6,6 @@ from . import assets
 import os.path
 
 
-class AdjustExpandRootDev(Task):
-	description = 'Adjusting the expand-root device'
-	phase = phases.system_modification
-	predecessors = [initd.AddExpandRoot, initd.AdjustExpandRootScript]
-
-	@classmethod
-	def run(cls, info):
-		from bootstrapvz.common.tools import sed_i
-		script = os.path.join(info.root, 'etc/init.d/expand-root')
-		sed_i(script, '/dev/xvda', '/dev/sda')
-
-
 class AddGrowRootDisable(Task):
 	description = 'Add script to selectively disable growroot'
 	phase = phases.system_modification
