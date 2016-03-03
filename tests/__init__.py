4a0c598b81ae85856ddef48f9fe1338daba6ed0e
Anders Ingemann <anders@ingemann.de>
2016-03-04 00:32:48 +0100

    Fix unit/manifests_tests
    The manifest folder reordering into official/ and examples/
    hid all of the manifests from that generator.
    recursive_glob has been introduced in order to be able to find all
    manifests.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+
+
+# Snatched from: http://stackoverflow.com/a/2186565/339505
+def recursive_glob(path, pattern):
+	import fnmatch
+	import os
+	for path, dirnames, filenames in os.walk(path):
+		for filename in fnmatch.filter(filenames, pattern):
+			yield os.path.join(path, filename)
