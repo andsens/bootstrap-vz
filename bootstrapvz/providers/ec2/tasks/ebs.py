673260ec63b0290c6f5d109ad8d2eeabfff76647
Marcin Kulisz <debian@kulisz.net>
2017-01-21 22:44:16 +0000

    EC2 provider: fixing snapshot tagging

--- bootstrapvz/providers/ec2/tasks/ebs.py
+++ bootstrapvz/providers/ec2/tasks/ebs.py
@@ -29,9 +29,10 @@ class Snapshot(Task):
     def run(cls, info):
         info._ec2['snapshot'] = info.volume.snapshot()
 
-#        # Setting up tags on the snapshot
-#        if 'tags' in info.manifest.data:
-#            raw_tags = info.manifest.data['tags']
-#            tags = {k: v.format(**info.manifest_vars) for k, v in raw_tags.items()}
-#            info._ec2['connection'](Resources=[info._ec2['snapshot']],
-#                                    Tags=[tags])
+        # Setting up tags on the snapshot
+        if 'tags' in info.manifest.data:
+            raw_tags = info.manifest.data['tags']
+            formatted_tags = {k: v.format(**info.manifest_vars) for k, v in raw_tags.items()}
+            tags = [{'Key': k, 'Value': v} for k, v in formatted_tags.items()]
+            info._ec2['connection'].create_tags(Resources=[info._ec2['snapshot']],
+                                                Tags=tags)
