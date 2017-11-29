fc569bcac55b9082bd1d806db60b10173b7671ea
Alex Hegedus <ahegedus@verizondigitalmedia.com>
2017-11-28 21:20:18 -0800

    Added password-cryped to root_password plugin

--- bootstrapvz/plugins/root_password/tasks.py
+++ bootstrapvz/plugins/root_password/tasks.py
@@ -11,9 +11,8 @@ class SetRootPassword(Task):
         from bootstrapvz.common.tools import log_check_call
         password_crypted = info.manifest.plugins['root_password'].get('password-crypted', None)
         if password_crypted is not None:
-            log_check_call(['chpasswd', '--root', info.root, '--encrypted'],
+            log_check_call(['chroot', info.root, '/usr/sbin/chpasswd', '--encrypted'],
                            'root:' + password_crypted)
         else:
-            log_check_call(['chroot', info.root, 'chpasswd'],
+            log_check_call(['chroot', info.root, '/usr/sbin/chpasswd'],
                            'root:' + info.manifest.plugins['root_password']['password'])
-
