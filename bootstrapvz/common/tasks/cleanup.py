6bb167f36144efb3ce94a9e2c385b8ee8b80fad8
Marcin Engelmann <mengelmann@octivi.com>
2018-01-11 17:46:00 +0100

    Clear the machine ID for jessie and newer releases (systemd) (resolves #427)

--- bootstrapvz/common/tasks/cleanup.py
+++ bootstrapvz/common/tasks/cleanup.py
@@ -30,3 +30,20 @@ class CleanTMP(Task):
         log = os.path.join(info.root, 'var/log/')
         os.remove(os.path.join(log, 'bootstrap.log'))
         os.remove(os.path.join(log, 'dpkg.log'))
+
+
+class ClearMachineId(Task):
+    description = 'Clearing the Machine ID'
+    phase = phases.system_cleaning
+
+    @classmethod
+    def run(cls, info):
+        import logging
+        log = logging.getLogger(__name__)
+        for machineid_file in [os.path.join(info.root, f) for f in ['etc/machine-id', 'var/lib/dbus/machine-id']]:
+            if os.path.isfile(machineid_file):
+                log.debug(machineid_file + ' found, clearing')
+                with open(machineid_file, 'w'):
+                    pass
+            else:
+                log.debug(machineid_file + ' not found, not clearing')
